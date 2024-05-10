36edc43386cf374622e6ad63533b5e10d9531c24
Henari Mualimin <74995107+henarimualimin@users.noreply.github.com>
2024-05-10 19:55:34 +0700

    Add files via upload

---
+++ api/index.tsx
@@ -0,0 +1,76 @@
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+import { serveStatic } from 'frog/serve-static'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/vercel'
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+          >             
+          {status === 'response'
+            ? `Nice choice.${buttonValue ?  `${buttonValue.toUpperCase()}!!` : ''}` // Mengganti fruit.toUpperCase() menjadi buttonValue.toUpperCase()
+            : 'Welcome! To Digger Bot'}
+              
+          </div>
+        </div>
+      ),
+    intents: [
+      <TextInput placeholder="Submit Your Username." />,
+      <Button value="Success" onClick={() => c.set({ buttonValue: 'Succes' })}>Submit</Button>,
+      <Button.Redirect value="https://warpcast.com/0xhen">Tip me</Button.Redirect>,
+      <Button.Redirect value="https://warpcast.com/~/compose?text=Frame_like_by_0xhen">Share</Button.Redirect>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== 'undefined'
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
+devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
